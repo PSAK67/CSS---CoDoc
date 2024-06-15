2e51ceedb3e40d4f15a637241b5e7ef73f54fab5
Talha Khubaib <talhadogar581@gmail.com>
2024-06-15 13:23:58 +0100

    Login Screen

---
+++ Login_Screen/scripts.js
@@ -0,0 +1,15 @@
+// document.getElementById('loginForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+//     var errorMessage = document.getElementById('errorMessage');
+
+//     // Dummy login validation
+//     if (username === 'admin' && password === 'password') {
+//         alert('Login successful!');
+//         // Redirect or perform other actions upon successful login
+//     } else {
+//         errorMessage.textContent = 'Invalid username or password';
+//     }
+// });
